d015cad9864368e470fddb691f3a55e069294dde
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-19 23:06:47 +0100

    Add loader component (#2)

--- src/apps/WebPage/src/app/app.module.ts
+++ src/apps/WebPage/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
