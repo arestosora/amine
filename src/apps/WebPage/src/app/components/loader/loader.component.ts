02a82ab26cccdb157d2758db15b570e18d9b68fb
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-20 10:33:21 +0100

    Update loader component (#3)

--- src/apps/WebPage/src/app/components/loader/loader.component.ts
+++ src/apps/WebPage/src/app/components/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-loader',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./loader.component.css']
 })
 export class LoaderComponent {
-
+  constructor(private loaderService:LoaderService) {}
+  /**
+   * Objeto que rige el comportamiento del loader
+   */
+  loader = {
+    /**
+     * Activa el loader y hazlo visible para todo el mundo
+     * @returns {undefined}
+     */
+    enable: ():undefined => {
+      return
+    },
+    /**
+     * Oculta el loader y destrúyelo, hazlo inaccesible
+     * @returns {undefined}
+     */
+    disable: ():undefined => {
+      return
+    },
+    /**
+     * Alterna el estado entre visible e invisible y devuelve si el loader es ahora visible o no
+     * @returns {Boolean}
+     */
+    toggle: ():Boolean => {
+      this.loaderService.visible?this.loader.disable():this.loader.enable()
+      return this.loaderService.visible
+    }
+  }
 }
