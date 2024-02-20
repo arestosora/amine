be9814a3fc9e64fa91bfae83135b4d18473f1b84
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-20 10:35:09 +0100

    Update app.components.ts (#3)

--- src/apps/WebPage/src/app/app.component.ts
+++ src/apps/WebPage/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { LoaderService } from './services/loader.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WebPage';
+export class AppComponent implements OnDestroy {
+  showLoader = false;
+  private loaderSubscription: Subscription;
+  title = "WebPage"
+
+  constructor(private loaderService: LoaderService) {
+    this.loaderSubscription = this.loaderService.loaderState$.subscribe((state) => {
+      this.showLoader = state;
+    });
+  }
+
+  ngOnDestroy() {
+    this.loaderSubscription.unsubscribe();
+  }
 }
