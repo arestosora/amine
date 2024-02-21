e23a2fead6fcb3ad154e9b73c399da54b487d5ca
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-21 11:30:42 +0100

    Update context-menu component (#3)

--- src/apps/WebPage/src/app/components/context-menu/context-menu.component.ts
+++ src/apps/WebPage/src/app/components/context-menu/context-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-context-menu',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./context-menu.component.css']
 })
 export class ContextMenuComponent {
+  isVisible: boolean = false;
+  menuPosition = { x: '0px', y: '0px' };
 
+  constructor(private router:Router) { }
+
+  @HostListener('document:contextmenu', ['$event'])
+  onRightClick(event: MouseEvent) {
+    event.preventDefault();
+    this.isVisible = true;
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const clickY = event.clientY;
+    const clickX = event.clientX;
+    const halfScreenHeight = screenHeight / 2;
+    if (clickY > halfScreenHeight) {
+      this.menuPosition.y = (clickY - 272) + 'px';
+    } else {
+      this.menuPosition.y = event.clientY + 'px';
+    }
+    const halfScreenWidth = screenWidth / 2;
+    if (clickX > halfScreenWidth) {
+      this.menuPosition.x = (clickX - 190) + 'px';
+    } else {
+      this.menuPosition.x = event.clientX + 'px';
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (this.isVisible) {
+      this.isVisible = false;
+    }
+  }
+
+  home() {
+    this.router.navigate([""])
+  }
+
+  pricing() {
+    this.router.navigate(["pricing"])
+  }
+
+  share() {
+    
+  }
+
+  back() {
+    window.history.back();
+  }
+
+  hideMenu() {
+    this.isVisible = false;
+  }
 }
