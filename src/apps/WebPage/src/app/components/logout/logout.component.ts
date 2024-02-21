e8de2f6d64ed7c17610704740f28f916d6ef496f
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-21 11:32:19 +0100

    Update login & logout (#3)

--- src/apps/WebPage/src/app/components/logout/logout.component.ts
+++ src/apps/WebPage/src/app/components/logout/logout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent {
-
+  constructor(private router:Router) {}
 }
