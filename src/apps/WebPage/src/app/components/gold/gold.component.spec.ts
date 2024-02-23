501501cdddc82fdb7f73520d4454e4778bfc0726
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-23 11:59:45 +0100

    Add gold component

---
+++ src/apps/WebPage/src/app/components/gold/gold.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GoldComponent } from './gold.component';
+
+describe('GoldComponent', () => {
+  let component: GoldComponent;
+  let fixture: ComponentFixture<GoldComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [GoldComponent]
+    });
+    fixture = TestBed.createComponent(GoldComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/apps/WebPage/src/app/components/gold/gold.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gold',
+  templateUrl: './gold.component.html',
+  styleUrls: ['./gold.component.css']
+})
+export class GoldComponent {
+
+}
