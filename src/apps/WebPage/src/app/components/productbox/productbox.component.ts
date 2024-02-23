b64db5c176580c46dd9d992c62d824353c20c648
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-23 12:27:29 +0100

    Update productbox component

--- src/apps/WebPage/src/app/components/productbox/productbox.component.ts
+++ src/apps/WebPage/src/app/components/productbox/productbox.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-productbox',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./productbox.component.css']
 })
 export class ProductboxComponent {
-
+  constructor(private router:Router) {}
+  go(page:string) {
+    this.router.navigate([page])
+  }
 }
