a6143c1b64971a0a787c3f4d49419fa93afb5660
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-23 12:01:16 +0100

    Update app-routing

--- src/apps/WebPage/src/app/app-routing.module.ts
+++ src/apps/WebPage/src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { PricingComponent } from './components/pricing/pricing.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
+import { CopperComponent } from './components/copper/copper.component';
+import { SilverComponent } from './components/silver/silver.component';
+import { GoldComponent } from './components/gold/gold.component';
+import { PrivacyComponent } from './components/privacy/privacy.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
-  },
-  {
-    path: "pricing",
-    component: PricingComponent
   },
   {
     path: "login",
@@ -21,6 +20,22 @@ const routes: Routes = [
   {
     path: "logout",
     component: LogoutComponent
+  },
+  {
+    path: "copper",
+    component: CopperComponent
+  },
+  {
+    path: "silver",
+    component: SilverComponent
+  },
+  {
+    path: "gold",
+    component: GoldComponent
+  },
+  {
+    path: "privacy",
+    component: PrivacyComponent
   }
 ];
 
