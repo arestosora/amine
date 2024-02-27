39f4bb64c69aa14c0bc6319abfea3ea14b44ebb5
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-02-27 12:34:02 +0100

    Update PaymentPage component (#8)

--- src/apps/WebPage/src/app/components/payment/payment.component.ts
+++ src/apps/WebPage/src/app/components/payment/payment.component.ts
@@ -6,5 +6,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentPage {
+  loader:boolean = false;
+  failed:boolean = false;
+  complete:boolean = false;
+  formMail:boolean = false;
+  formPersonal:boolean = false;
+  formPlan:boolean = false;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.loader = true;
+    this.failed = false;
+    this.complete = false;
+    this.formMail = false;
+    this.formPersonal = false;
+    this.formPlan = false;
+  }
+
+  loadFailed() {
+    this.loader = false;
+    this.failed = true;
+    this.complete = false;
+    this.formMail = false;
+    this.formPersonal = false;
+    this.formPlan = false;
+  }
+
+  loadCompleted() {
+    this.loader = false;
+    this.failed = false;
+    this.complete = true;
+    this.formMail = false;
+    this.formPersonal = false;
+    this.formPlan = false;
+    this.loadFormMail()
+  }
+
+  loadFormMail() {
+    this.loader = false;
+    this.failed = false;
+    this.complete = false;
+    this.formMail = true;
+    this.formPersonal = false;
+    this.formPlan = false;
+  }
+
+  loadFormPersonal() {
+    this.loader = false;
+    this.failed = false;
+    this.complete = false;
+    this.formMail = false;
+    this.formPersonal = true;
+    this.formPlan = false;
+  }
+
+  loadFormPlan() {
+    this.loader = false;
+    this.failed = false;
+    this.complete = false;
+    this.formMail = false;
+    this.formPersonal = false;
+    this.formPlan = true;
+  }
 }
