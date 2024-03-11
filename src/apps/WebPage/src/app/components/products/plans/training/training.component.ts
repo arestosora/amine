df339ffce14d43a588d3696f8c015db9db492cd7
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-03-11 23:17:02 +0100

    Add training banner component

---
+++ src/apps/WebPage/src/app/components/products/plans/training/training.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-plan-training',
+  templateUrl: './training.component.html',
+  styleUrls: ['./training.component.css']
+})
+export class TrainingComponent {
+
+}
