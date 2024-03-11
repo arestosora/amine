ff58ea75b02bbddf4fb13a3975dab51901329c19
Gabriel Carro <73355508+gacarbla@users.noreply.github.com>
2024-03-11 23:16:22 +0100

    Add alimentation banner component

---
+++ src/apps/WebPage/src/app/components/products/plans/alimentation/alimentation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-plan-alimentation',
+  templateUrl: './alimentation.component.html',
+  styleUrls: ['./alimentation.component.css']
+})
+export class AlimentationComponent {
+
+}
